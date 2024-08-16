968b3ee7aee935f99c5d6251fc0cc3464459a3eb
Magnus <mag.face123@gmail.com>
2024-08-16 10:30:11 +0200

    fix webshell/cracking double label

--- labeler.py
+++ labeler.py
@@ -17,53 +17,54 @@ scenario_options = [
 ]
 
 noise_messages = [
-"Web server 400 error code.",
-"IDS event.",
-"Dovecot Authentication Success.",
-"Multiple web server 400 error codes from same source ip.",
-"Suricata: Alert - SURICATA TLS invalid record/traffic",
-"Suricata: Alert - SURICATA TLS invalid handshake message",
-"Suricata: Alert - ET INFO Observed DNS Query to .biz TLD",
-"ClamAV database update",
-"Multiple IDS alerts for same id.",
-"Suricata: Alert - ET POLICY GNU/Linux APT User-Agent Outbound likely related to package management",
-"Multiple IDS events from same source ip.",
-"Multiple IDS alerts for same id (ignoring now this id).",
-"Dovecot Invalid User Login Attempt.",
-"PAM: User login failed.",
-"First time this IDS alert is generated.",
-"Suricata: Alert - ET INFO Observed DNS Query to .cloud TLD",
-"syslog: User authentication failure.",
-"CMS (WordPress or Joomla) login attempt.",
-"Suricata: Alert - SURICATA HTTP unable to match response to request",
-"Apache: Attempt to access forbidden directory index.",
-"Multiple IDS events from same source ip (ignoring now this srcip and id).",
-"PAM: Login session opened.",
-"Auditd: SELinux permission check.",
-"PAM: Login session closed.",
-"Suricata: Alert - ET DNS Query for .to TLD",
-"Web server 500 error code (Internal Error).",
-"Suricata: Alert - ET DNS Query for .cc TLD",
-"Suricata: Alert - ET HUNTING Possible COVID-19 Domain in SSL Certificate M2",
-"sshd: authentication success.",
-"Suricata: Alert - SURICATA HTTP gzip decompression failed",
-"Suricata: Alert - ET HUNTING Suspicious Domain Request for Possible COVID-19 Domain M1",
-"Successful sudo to ROOT executed.",
-"Suricata: Alert - ET HUNTING Suspicious TLS SNI Request for Possible COVID-19 Domain M1",
-"Suricata: Alert - ET INFO DNS Query for Suspicious .ga Domain",
-"Suricata: Alert - ET INFO Suspicious Domain (*.ga) in TLS SNI",
-"Suricata: Alert - SURICATA TLS invalid SSLv2 header",
-"Suricata: Alert - SURICATA SMTP no server welcome message",
-"Suricata: Alert - SURICATA SMTP invalid reply",
-"First time user executed sudo.",
-"Suricata: Alert - SURICATA HTTP invalid response chunk len",
-"Suricata: Alert - ET INFO TLS Handshake Failure",
-"Suricata: Alert - ET INFO Session Traversal Utilities for NAT (STUN Binding Request)",
-"Suricata: Alert - SURICATA TLS invalid record type",
-"User successfully changed UID.",
-"Suricata: Alert - SURICATA TLS certificate invalid der",
-"Suricata: Alert - SURICATA DNS Unsolicited response",
+    "Web server 400 error code.",
+    "IDS event.",
+    "Dovecot Authentication Success.",
+    "Multiple web server 400 error codes from same source ip.",
+    "Suricata: Alert - SURICATA TLS invalid record/traffic",
+    "Suricata: Alert - SURICATA TLS invalid handshake message",
+    "Suricata: Alert - ET INFO Observed DNS Query to .biz TLD",
+    "ClamAV database update",
+    "Multiple IDS alerts for same id.",
+    "Suricata: Alert - ET POLICY GNU/Linux APT User-Agent Outbound likely related to package management",
+    "Multiple IDS events from same source ip.",
+    "Multiple IDS alerts for same id (ignoring now this id).",
+    "Dovecot Invalid User Login Attempt.",
+    "PAM: User login failed.",
+    "First time this IDS alert is generated.",
+    "Suricata: Alert - ET INFO Observed DNS Query to .cloud TLD",
+    "syslog: User authentication failure.",
+    "CMS (WordPress or Joomla) login attempt.",
+    "Suricata: Alert - SURICATA HTTP unable to match response to request",
+    "Apache: Attempt to access forbidden directory index.",
+    "Multiple IDS events from same source ip (ignoring now this srcip and id).",
+    "PAM: Login session opened.",
+    "Auditd: SELinux permission check.",
+    "PAM: Login session closed.",
+    "Suricata: Alert - ET DNS Query for .to TLD",
+    "Web server 500 error code (Internal Error).",
+    "Suricata: Alert - ET DNS Query for .cc TLD",
+    "Suricata: Alert - ET HUNTING Possible COVID-19 Domain in SSL Certificate M2",
+    "sshd: authentication success.",
+    "Suricata: Alert - SURICATA HTTP gzip decompression failed",
+    "Suricata: Alert - ET HUNTING Suspicious Domain Request for Possible COVID-19 Domain M1",
+    "Successful sudo to ROOT executed.",
+    "Suricata: Alert - ET HUNTING Suspicious TLS SNI Request for Possible COVID-19 Domain M1",
+    "Suricata: Alert - ET INFO DNS Query for Suspicious .ga Domain",
+    "Suricata: Alert - ET INFO Suspicious Domain (*.ga) in TLS SNI",
+    "Suricata: Alert - SURICATA TLS invalid SSLv2 header",
+    "Suricata: Alert - SURICATA SMTP no server welcome message",
+    "Suricata: Alert - SURICATA SMTP invalid reply",
+    "First time user executed sudo.",
+    "Suricata: Alert - SURICATA HTTP invalid response chunk len",
+    "Suricata: Alert - ET INFO TLS Handshake Failure",
+    "Suricata: Alert - ET INFO Session Traversal Utilities for NAT (STUN Binding Request)",
+    "Suricata: Alert - SURICATA TLS invalid record type",
+    "User successfully changed UID.",
+    "Suricata: Alert - SURICATA TLS certificate invalid der",
+    "Suricata: Alert - SURICATA DNS Unsolicited response",
 ]
+
 
 def check_sequence(filename):
     """
@@ -112,10 +113,20 @@ def label_aminer(filename, attack_times, dataset_dir, output_dir):
 
             detection_time = int(obj["LogData"]["DetectionTimestamp"][0])
 
-            obj["Label"] = []
+            possible_labels = []
             for attack, start, end in attack_times:
                 if start <= detection_time <= end:
-                    obj["Label"].append(attack)
+                    possible_labels.append(attack)
+
+            if len(possible_labels) > 1:
+                print("Multiple labels found for line", obj)
+                correct_label = find_correct_label(possible_labels, obj)
+                print("correct label", correct_label)
+                obj["Label"] = correct_label
+            elif len(possible_labels) == 1:
+                obj["Label"] = possible_labels[0]
+            else:
+                obj["Label"] = "benign"
 
             output_file.write(json.dumps(obj) + "\n")
     output_file.close()
@@ -128,7 +139,7 @@ def label_wazuh(filename, attack_times, dataset_dir, output_dir):
             obj = json.loads(line)
 
             # if remove_noise argument in argumentParser() is stored as true, then remove the noise messages
-            
+
             if arguments.remove_noise:
                 if obj["rule"]["description"] in noise_messages:
                     continue
@@ -151,6 +162,25 @@ def label_wazuh(filename, attack_times, dataset_dir, output_dir):
     output_file.close()
 
 
+def find_correct_label(labels: list, alert_obj):
+    if "webshell" in labels and "cracking" in labels:
+        try:
+            if "/intranet-access.log" in alert_obj["LogData"]["LogResources"][0]:
+                return "webshell"
+            else:
+                return "cracking"
+        except KeyError:
+            print("🚨 Failed while resolving webshell vs cracking with labels", labels)
+
+    if "wpscan" in labels and "dirb" in labels:
+        pass
+        # labels.remove("dirb")
+
+    if "reverse_shell" in labels and "privilege_escalation" in labels:
+        pass
+        # labels.remove("privilege_escalation")
+
+
 def full_convert(
     scenario_name, label_filename, dataset_dir, output_dir, start_offset, end_offset
 ):
@@ -169,12 +199,7 @@ def full_convert(
 
     sucess = True
     try:
-        label_aminer(
-            aminer_filename,
-            attack_times,
-            dataset_dir,
-            output_dir
-        )
+        label_aminer(aminer_filename, attack_times, dataset_dir, output_dir)
         print(
             "✅ Created new file:",
             output_dir + "/labeled_" + scenario_name + "_aminer.json",
@@ -185,12 +210,7 @@ def full_convert(
         sucess = False
 
     try:
-        label_wazuh(
-            wazuh_filename,
-            attack_times,
-            dataset_dir,
-            output_dir
-        )
+        label_wazuh(wazuh_filename, attack_times, dataset_dir, output_dir)
         print(
             "✅ Created new file:",
             output_dir + "/labeled_" + scenario_name + "_wazuh.json",
